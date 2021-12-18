7c189ffae40901ee26597e6c36122331ab5ec8d2
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-18 10:35:25 +0100

    query

---
+++ apps/graph_commons/lib/graph_commons/query.ex
@@ -0,0 +1,58 @@
+# ---
+# Excerpted from "Exploring Graphs with Elixir",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/thgraphs for more book information.
+# ---
+defmodule GraphCommons.Query do
+  ## ATTRIBUTES
+
+  @storage_dir GraphCommons.storage_dir()
+
+  ## STRUCT
+
+  @derive {Inspect, except: [:path, :uri]}
+
+  @enforce_keys ~w[data file type]a
+  defstruct ~w[data file path type uri]a
+
+  ## TYPES
+
+  @type query_data :: String.t()
+  @type query_file :: String.t()
+  @type query_path :: String.t()
+  @type query_type :: GraphCommons.query_type()
+  @type query_uri :: String.t()
+
+  @type t :: %__MODULE__{
+          # user
+          data: query_data,
+          file: query_file,
+          type: query_type,
+          # system
+          path: query_path,
+          uri: query_uri
+        }
+
+  defguard is_query_type(query_type)
+           when query_type in [:dgraph, :native, :property, :rdf, :tinker]
+
+  ## CONSTRUCTOR
+
+  def new(query_data, query_file, query_type)
+      when is_query_type(query_type) do
+    query_path = "#{@storage_dir}/#{query_type}/queries/#{query_file}"
+
+    %__MODULE__{
+      # user
+      data: query_data,
+      file: query_file,
+      type: query_type,
+      # system
+      path: query_path,
+      uri: "file://" <> query_path
+    }
+  end
+end
