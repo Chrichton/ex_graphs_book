c3e495b24d1b5eb438ae9b37b942d02ebc7bc34c
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-18 08:03:48 +0100

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,11 +3,27 @@ defmodule ExGraphsBook.MixProject do
 
   def project do
     [
+      aliases: aliases(),
+      apps: [
+        :graph_commons,
+        :native_graph,
+        :property_graph,
+        :rdf_graph,
+        :tinker_graph
+      ],
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
+  end
+
+  defp aliases do
+    []
   end
 
   # Dependencies listed here are available only for this
